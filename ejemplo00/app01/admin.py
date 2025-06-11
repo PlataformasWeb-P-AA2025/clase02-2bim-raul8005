331a8bcb921c11d39fcb261c2b89835dfd9b1e9e
raul8005 <rimedina@utpl.edu.ec>
2025-06-11 08:50:49 -0500

    Cambios django

--- ejemplo00/app01/admin.py
+++ ejemplo00/app01/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from app01.models import *
+
+admin.site.register(Estudiante)
+admin.site.register(Ciudad)
--- ejemplo00/app01/models.py
+++ ejemplo00/app01/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField()
+
+def __str__(self):
+    return "%s - %s - %s - %d" % (self.nombre,
+            self.apellido,
+            self.cedula,
+            self.edad)
+
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+    poblacion = models.IntegerField()
+
+def __str__(self):
+    return "%s - %d" % (self.nombre ,
+            self.poblacion)
